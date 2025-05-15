21e3d5edc25ebc1e3db2b56c5b48441677c4d59a
pravinj28 <pravinj0611@gmail.com>
2025-05-15 16:43:48 +0530

    Update Data_Manager.py
    1. Loads environment variables for Sheety API credentials and endpoints using dotenv.
    2.Initializes a DataManager class to store destination and customer data with HTTP Basic Auth.

--- Data_Manager.py
+++ Data_Manager.py
@@ -1,1 +1,22 @@
+import os
+from pprint import pprint
+import requests
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+class DataManager:
+
+    def __init__(self):
+
+        self._user = os.environ["SHEETY_USERNAME"]
+        self._password = os.environ["SHEETY_PASSWORD"]
+        # Save your Sheety endpoints an environment variables
+        self.prices_endpoint = os.environ["SHEETY_PRICES_ENDPOINT"]
+        self.users_endpoint = os.environ["SHEETY_USERS_ENDPOINT"]
+        self._authorization = HTTPBasicAuth(self._user, self._password)
+        # Destination and Customer fields data start out empty
+        self.destination_data = {}
+        self.customer_data = {}
