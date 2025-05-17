6bd5ac258a2113bed035ce3acbfb53a0ac77bc9e
Key88ux <kunyajankar8@gmail.com>
2025-05-17 11:20:10 +0530

    Flight_Search.py
    1. Authenticate using OAuth2 to obtain an access token.
    
    2. Use the token to query city and airport information using a city name .

--- Flight_Search.py
+++ Flight_Search.py
@@ -1,1 +1,12 @@
+import requests
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 
